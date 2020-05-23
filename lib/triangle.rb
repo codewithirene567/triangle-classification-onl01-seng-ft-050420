b238239cd324706d521a6d264eb0ee7c7ab7b93b
Irene Scott <irenejinscott@gmail.com>
2020-05-23 00:04:17 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,9 +1,13 @@
 class Triangle
+attr_accessor :length_first, :length_second, :length_third)
   def initialize(length_first, length_second, length_third)
     @length_first = length_first
     @length_second = length_second
     @length_third = length_third
   end
 
-  def
+  def kind
+    :equilateral
+    #returns its type as a symbol
+  end
 end
