7df8f38598fb968331f1649e0becfe92cd186b39
Irene Scott <irenejinscott@gmail.com>
2020-05-23 00:02:48 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,5 +1,9 @@
 class Triangle
   def initialize(length_first, length_second, length_third)
-    
+    @length_first = length_first
+    @length_second = length_second
+    @length_third = length_third
   end
+
+  def 
 end
