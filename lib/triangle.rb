d494861cc135391edea813eb6d57288d8cecfc5c
Irene Scott <irenejinscott@gmail.com>
2020-05-23 00:20:35 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,7 +11,7 @@ attr_accessor :length_first, :length_second, :length_third)
     :isosceles == length_first == length_second == !length_third
     :scalene == !length_first == !length_second == !length_third
     #returns its type as a symbol
-   if side.Triangle
+   if side.Triangle #does not equal any of these types
     begin
     raise TriangleError
     rescue
